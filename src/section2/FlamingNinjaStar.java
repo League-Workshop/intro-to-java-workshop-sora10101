76a4183f1de9ac1d2377479a8de0f4153c73f659
League Student <league@iMac-12.attlocal.net>
2019-07-12 14:28:37 -0700

    :+1:

--- src/section2/FlamingNinjaStar.java
+++ src/section2/FlamingNinjaStar.java
@@ -20,21 +20,21 @@ Robot rob= new Robot();
 		// 13. Use a for loop to repeat all of the code below 25 times
 
 			// 2. Turn the robot 1/8 of a circle
-		rob.turn(degrees);
+		rob.turn(1/8);
 			// 3. Move the robot 64 pixels
-
+rob.move(64);
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			rob.turn(40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			rob.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			rob.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			rob.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+			rob.turn(64);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			rob.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			
 			// 11. Color your ninja star like Figure 2.
